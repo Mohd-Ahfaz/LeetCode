176da007ce2c92b3be07776118fd9823c4441e88
Mohd-Ahfaz <mohdahfaz180@gmail.com>
2026-09-06 15:02:25 +0530

    Solved Binary Search Problem

---
+++ Arrays/Binary Search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid=0;
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r){
+            mid = (l + r) / 2;
+            if(nums[mid]<target) l = mid + 1;
+            else if(nums[mid]>target) r = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
+};
