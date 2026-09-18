cdb78e79f3085e0a735a6675a566ce273f5d9cd0
Mohd-Ahfaz <mohdahfaz180@gmail.com>
2026-09-18 15:13:20 +0530

    Solved Remove Nth Node From End of List Problem

---
+++ Linked List, Stack , Queue/Remove Nth Node From End of List.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        for(int i = 0; i < n; i++) fast = fast->next;
+        if(fast == NULL) return head->next;
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delNode = NULL;
+        return head;
+    }
+};
